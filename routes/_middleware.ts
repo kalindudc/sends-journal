3e516289d6af65fd82c23db033e963bf45f0df8c
Kalindu De Costa <kalinduk.decosta@gmail.com>
2022-11-21 17:17:03 -0500

    Add deno and todos

---
+++ routes/_middleware.ts
@@ -0,0 +1,7 @@
+import { remultFresh } from "remult/remult-fresh";
+import { Task } from "../model/task.ts";
+
+export const remultServer = remultFresh({
+  entities: [Task]
+}, Response);
+export const handler = remultServer.handle;
